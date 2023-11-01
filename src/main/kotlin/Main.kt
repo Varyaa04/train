eb1837c07ff14cda937138b5b92078ccab56a7a0
Varushka ☆彡 <fedorova04_varya@mail.ru>
2023-11-01 19:51:39 +0300

    train

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,7 +1,57 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import kotlin.random.Random
+fun main() {
+    var continueWork = true
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
+    while (continueWork) {
+        println("Выберите действие:")
+        println("1. Создать направление")
+        println("2. Продать билеты")
+        println("3. Сформировать поезд")
+        println("4. Отправить поезд")
+        println("5. Закончить работу")
+
+        val choice = readLine()?.toIntOrNull()
+
+        when (choice) {
+            1 -> createDirection()
+            2 -> sellTickets()
+            3 -> formTrain()
+            4 -> sendTrain()
+            5 -> continueWork = false
+            else -> println("Некорректный выбор. Попробуйте еще раз.")
+        }
+    }
+}
+
+private fun createDirection() {
+    val cities = listOf("Бийск","Москва","Санкт-Петербург", "Барнаул", "Новосибирск", "Омск", "Томск", "Кемерово", "Новокузнецк", "Красноярск", "Иркутск", "Оренбург", "Челябинск", "Екатеринбург", "Пермь", "Уфа", "Казань")
+    val randomCities = cities.shuffled().take(2)
+    val startCity = randomCities[0]
+    val endCity = randomCities[1]
+
+    println("Направление создано: $startCity - $endCity")
+}
+
+private fun sellTickets() {
+    val passengers = Random.nextInt(5, 201)
+
+    println("Продано билетов: $passengers")
+}
+
+private fun formTrain() {
+    val passengers = Random.nextInt(5, 201)
+    val train = mutableListOf<Int>()
+    var capacitySum = 0
+
+    while (capacitySum < passengers) {
+        val capacity = Random.nextInt(5, 26)
+        train.add(capacity)
+        capacitySum += capacity
+    }
+
+    println("Поезд сформирован: $train")
+}
+
+private fun sendTrain() {
+    println("Поезд отправлен")
 }
